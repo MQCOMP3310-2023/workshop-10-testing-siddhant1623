d83a635091db5fb559d39cd0df6ec3781e393322
priyam.kandiban2021MQ46583653 <priyam.kandiban@students.mq.edu.au>
2023-05-15 16:27:52 +1000

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -8,7 +8,7 @@ from werkzeug.security import check_password_hash
 class TestWebApp(unittest.TestCase):
     def setUp(self):
         self.app = create_app({
-            "SQLALCHEMY_DATABASE_URI": 'sqlite://'} )
+            "SQLALCHEMY_DATABASE_URI": 'sqlite://'})
         self.app.config['WTF_CSRF_ENABLED'] = False  # no CSRF during tests
         self.appctx = self.app.app_context()
         self.appctx.push()
@@ -27,7 +27,7 @@ class TestWebApp(unittest.TestCase):
         assert current_app == self.app
 
     def test_homepage_redirect(self):
-        response = self.client.get('/', follow_redirects = True)
+        response = self.client.get('/', follow_redirects=True)
         assert response.status_code == 200
 
     def test_registration_form(self):
@@ -36,36 +36,36 @@ class TestWebApp(unittest.TestCase):
 
     def test_no_access_to_profile(self):
         # TODO: Check that non-logged-in user should be redirected to /login
-        response = self.client.get('/profile' ,
-                                   follow_redirects = True)
+        response = self.client.get('/profile',
+                                   follow_redirects=True)
         assert response.status_code == 200
         assert response.request.path == '/login'
 
     def test_register_user(self):
-        response = self.client.post('/signup', data = {
-            'email' : 'user@test.com',
-            'name' : 'test user',
-            'password' : 'test123'
-        }, follow_redirects = True)
+        response = self.client.post('/signup', data={
+            'email': 'user@test.com',
+            'name': 'test user',
+            'password': 'test123'
+        }, follow_redirects=True)
         assert response.status_code == 200
         # should redirect to the login page
         assert response.request.path == '/login'
 
         # verify that user can now login
-        response = self.client.post('/login', data = {
-            'email' : 'user@test.com',
-            'password' : 'test123'
-        }, follow_redirects = True)
+        response = self.client.post('/login', data={
+            'email': 'user@test.com',
+            'password': 'test123'
+        }, follow_redirects=True)
         assert response.status_code == 200
-        html = response.get_data(as_text = True)
+        html = response.get_data(as_text=True)
         assert 'test user' in html
 
     def test_hashed_passwords(self):
-        response = self.client.post('/signup', data = {
-            'email' : 'user@test.com',
-            'name' : 'test user',
-            'password' : 'test123'
-        }, follow_redirects = True)
+        response = self.client.post('/signup', data={
+            'email': 'user@test.com',
+            'name': 'test user',
+            'password': 'test123'
+        }, follow_redirects=True)
         assert response.status_code == 200
         # should redirect to the login page
         assert response.request.path == '/login'
@@ -75,28 +75,28 @@ class TestWebApp(unittest.TestCase):
         assert check_password_hash(user.password, 'test123')
 
     def test_sql_injection(self):
-        response = self.client.post('/signup', data = {
-            'email' : 'user@test.com"; drop table user; -- ',
-            'name' : 'test user',
-            'password' : 'test123'
-        }, follow_redirects = True)
-        assert response.status_code == 200 
-
-    def test_xss_vulnerability(self):
-        response = self.client.post('/signup', data ={
-            'email' : 'user@test.com',
-            'name' : '<script> alert("Hello");</script>',
-            'password' : 'test123'
-        },follow_redirects=True)
+        response = self.client.post('/signup', data={
+            'email': 'user@test.com"; drop table user; -- ',
+            'name': 'test user',
+            'password': 'test123'
+        }, follow_redirects=True)
         assert response.status_code == 200
 
-        #verify that the user can now login
-        response == self.client.post('/login', data={
-            'email' : 'user@test.com',
-            'password' : 'test123'
+    def test_xss_vulnerability(self):
+        response = self.client.post('/signup', data={
+            'email': 'user@test.com',
+            'name': '<script> alert("Hello");</script>',
+            'password': 'test123'
         }, follow_redirects=True)
-        assert response.status_code== 200
+        assert response.status_code == 200
+
+        # verify that the user can now login
+        response == self.client.post('/login', data={
+            'email': 'user@test.com',
+            'password': 'test123'
+        }, follow_redirects=True)
+        assert response.status_code == 200
         html = response.get_data(as_text=True)
         assert not '<script>' in html
 
-
+# i added this comment - pridogs
